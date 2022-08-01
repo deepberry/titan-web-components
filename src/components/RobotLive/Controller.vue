866386705d00fde9465d245b8e41d0601540041c
iRuxu <17229398@qq.com>
2022-08-01 18:16:22 +0800

    feat:robot live controller

---
+++ src/components/RobotLive/Controller.vue
@@ -0,0 +1,32 @@
+<!-- 方向盘控制UI -->
+<template>
+    <div class="w-controller">
+        <el-button type="primary" plain :loading="state.left" @click="command('left')">←</el-button>
+        <el-button type="primary" plain :loading="state.stop" @click="command('stop')">=</el-button>
+        <el-button type="primary" plain :loading="state.right" @click="command('right')">→</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ControllerPanel",
+    props: {
+        state: {
+            type: Object,
+            default: () => ({
+                left: false,
+                right: false,
+                top: false,
+                bottom: false,
+                stop: false,
+            }),
+        },
+    },
+    emits: ["command"],
+    methods: {
+        command: function (command) {
+            this.$emit("command", command);
+        },
+    },
+};
+</script>
---
+++ src/components/RobotLive/RobotLive.vue
@@ -0,0 +1,67 @@
+<!--
+    控制轨道机器人进行移动，并直播展示给用户
+
+
+ -->
+<template>
+    <div class="w-live w-live-aliyun">
+        <!-- 视频直播 -->
+        <div class="w-live-video">
+            <!-- 播放器 -->
+            <div id="J_prismPlayer"></div>
+            <!-- 信息 -->
+            <div class="u-meta">
+                <div class="u-meta-time">当前时间</div>
+            </div>
+        </div>
+
+        <!-- 控制器 -->
+        <div class="w-live-controller">
+            <!-- 机器人控制 -->
+            <Controller class="u-robot-controller" :state="robotControllerState" @command="execCommand" />
+            <!-- 相机控制 -->
+            <Controller class="u-camera-controller" :state="cameraControllerState" @command="execCommand" />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+// import * as signalR from "@microsoft/signalr";
+// const { SOCKET_API_URL } = import.meta.env;
+</script>
+
+<script lang="ts">
+import Controller from "./Controller.vue";
+export default {
+    name: "RobotLive",
+    props: ["property"],
+    components: {
+        Controller,
+    },
+    data: function () {
+        return {
+            // 机器人控制状态
+            robotControllerState: {
+                left: false, //左
+                right: false, //右
+                top: false, //前
+                bottom: false, //后
+                stop: false, //停止
+            },
+            // 相机控制状态
+            cameraControllerState: {
+                left: false, //左转
+                right: false, //右转
+                top: false, //上调
+                bottom: false, //下调
+                stop: false, //停止
+            },
+        };
+    },
+    computed: {},
+    watch: {},
+    methods: {},
+    created: function () {},
+    mounted: function () {},
+};
+</script>
