3d63f6c453c715c66f23cf474a90502c2452e45f
iRuxu <17229398@qq.com>
2022-08-03 18:06:31 +0800

    feat:locale part

---
+++ src/locale/lang/en-us.ts
@@ -0,0 +1,13 @@
+export default {
+    RobotLive: {
+        Controller: {
+            left: "Left",
+            right: "Right",
+            top: "Top OR Forward",
+            bottom: "Bottom OR Backward",
+            stop: "Stop",
+            zoomIn: "Zoon In",
+            zoomOut: "Zoom Out",
+        },
+    },
+};
---
+++ src/locale/lang/zh-cn.ts
@@ -0,0 +1,13 @@
+export default {
+    RobotLive: {
+        Controller: {
+            left: "向左移动或转动",
+            right: "向右移动或转动",
+            top: "向上或前进",
+            bottom: "向下或后退",
+            stop: "停止移动或转动",
+            zoomIn: "镜头放远",
+            zoomOut: "放大放近",
+        },
+    },
+};
