b2d13b069bc00829a62d13f1e358489ef2a4b938
fifthThirteen <570205219@qq.com>
2025-07-10 17:16:18 +0800

    fix:account

--- src/service/account.js
+++ src/service/account.js
@@ -29,35 +29,35 @@ export function switchOrg(id) {
 
 // 更新偏好设置
 export function updatePreference(data) {
-    return $titan().put(`/user/preference`, data);
+    return $titan().put(`user/preference`, data);
 }
 
 // 获取时区
 export function getTimeZoneList(params) {
-    return $titan().get("/conf/timezone", { params });
+    return $titan().get("conf/timezone", { params });
 }
 
 // 消息中心 获取用户未读消息
 export function getMessageUnRead() {
-    return $titan().get(`/user/message/count`);
+    return $titan().get(`user/message/count`);
 }
 
 // 联系客服提交quote
 export const addQuotation = (data) => {
-    return $service().post("/www/quotation", data);
+    return $service().post("www/quotation", data);
 };
 
 // 获取菜单栏
 export const getMenus = (params) => {
-    return $titan().get(`/system/menu`, { params });
+    return $titan().get(`system/menu`, { params });
 };
 
 // 获取用户调查记录
 export const getSurveys = () => {
-    return $titan().get(`/user/survey`);
+    return $titan().get(`user/survey`);
 };
 
 // 用户提交调查
 export const submitSurvey = (id, data) => {
-    return $titan().post(`/user/survey/${id}`, data);
+    return $titan().post(`user/survey/${id}`, data);
 };
